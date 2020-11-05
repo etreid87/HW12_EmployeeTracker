ab6a4912fd15222e49835d87bc2055f99a909508
Evan Reid <etreid87@gmail.com>
2020-11-04 19:33:46 -0500

    prompt

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-var mysql = require("mysql");
-require("console.table");
+// require("console.table");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -15,3 +14,50 @@ connection.connect(function (err) {
   if (err) throw err;
   initialPrompt();
 });
+function initialPrompt() {
+    inquirer.prompt({
+        message: "what would you like to do?",
+        type: "list",
+        choices: [
+            "View All Employees",
+            "View All Departments",
+            "Add Employee",
+            "Add Department",
+            "Add Role",
+            "Update Employee Role",
+            "EXIT"
+        ],
+        name: "choice"
+    }).then(answers => {
+        console.log(answers.choice);
+        switch (answers.choice) {
+            case "View All Employees":
+                viewEmployees()
+                break;
+
+            case "View All Departments":
+                viewDepartments()
+                break;
+
+            case "Add Employee":
+                addEmployee()
+                break;
+
+            case "Add Department":
+                addDepartment()
+                break;
+
+            case "Add Role":
+                addRole()
+                break;
+
+            case "Update Employee Role":
+                updateEmployeeRole();
+                break;
+
+            default:
+                connection.end()
+                break;
+        }
+    })
+}
