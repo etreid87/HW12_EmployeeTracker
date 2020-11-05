58f41338ae29be5ec0a76c5bc8d7fc5f753bd731
Evan Reid <etreid87@gmail.com>
2020-11-04 21:02:33 -0500

    departments

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const inquirer = require("inquirer");
 // require("console.table");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Oasis185",
-  database: "employeeDB"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Oasis185",
+    database: "employeeDB"
 });
 
 connection.connect(function (err) {
-  if (err) throw err;
-  initialPrompt();
+    if (err) throw err;
+    initialPrompt();
 });
 function initialPrompt() {
     inquirer.prompt({
@@ -61,53 +61,92 @@ function initialPrompt() {
         }
     })
 }
-function addEmployee() {
-    console.log("Inserting a new employee.\n");
-    inquirer 
-      .prompt ([ 
-        {
-          type: "input", 
-          message: "First Name?",
-          name: "first_name",
-        },
-        {
-          type: "input", 
-          message: "Last Name?",
-          name: "last_name"
-        },
-        {
-          type: "list",
-          message: "What is the employee's role?",
-          name: "role_id", 
-          choices: [1,2,3]
-        },
-        {
-          type: "input", 
-          message: "Who is their manager?",
-          name: "manager_id"
-        }
-      ])
-      .then (function(res){
-        const query = connection.query(
-          "INSERT INTO employeeDB SET ?", 
-         res,
-          function(err, res) {
-            if (err) throw err;
-            console.log( "Employee added!\n");
-    
-            start (); 
-          }
-        );    
-      })
-    }
-    function viewEmployees() {
-    
-      connection.query("SELECT employee.first_name, employee.last_name, roles.title AS \"role\", managers.first_name AS \"manager\" FROM employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN employees managers ON employees.manager_id = managers.id GROUP BY employees.id",  
-      function(err, res) {
+function viewDepartments() {
+    connection.query("SELECT * FROM departments", function (err, res) {
         if (err) throw err;
         console.table(res);
-        start();
-      });
-    }
-    
-    
+        initialPrompt
+    });
+};
+function addDepartment() {
+    connection.query()
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is the name of the department you would like to add?",
+                name: "new_department"
+            }
+        ])
+        .then(function (res) {
+            console.log(res);
+            const query = connection.query(
+                "INSERT INTO departments SET ?",
+                {
+                    name: res.new_department
+                },
+                function (err, res) {
+                    connection.query("SELECT * FROM departments", function (err, res) {
+                        console.table(res);
+                        initialPrompt();
+                    })
+                }
+            )
+        })
+}
+
+
+function addEmployee() {
+    console.log("Inserting a new employee.\n");
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "First Name?",
+                name: "first_name",
+            },
+            {
+                type: "input",
+                message: "Last Name?",
+                name: "last_name"
+            },
+            {
+                type: "list",
+                message: "What is the employee's role?",
+                name: "role_id",
+                choices: [1, 2, 3]
+            },
+            {
+                type: "input",
+                message: "Who is their manager?",
+                name: "manager_id"
+            }
+        ])
+        .then(function (res) {
+            const query = connection.query(
+                "INSERT INTO employeeDB SET ?",
+                res,
+                function (err, res) {
+                    if (err) throw err;
+                    console.log("Employee added\n");
+
+                    initialPrompt();
+                }
+            );
+        })
+}
+function viewEmployees() {
+
+    connection.query("SELECT employee.first_name, employee.last_name, roles.title AS \"role\", managers.first_name AS \"manager\" FROM employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN employees managers ON employees.manager_id = managers.id GROUP BY employees.id",
+        function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            initialPrompt();
+        });
+}
+function addRole() {
+    inquirer
+    .prompt([
+        
+    ])
+}
